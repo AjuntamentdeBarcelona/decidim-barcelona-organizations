cb24da7669f77ec5e8f1dba7c24f19636dab5662
Josep Jaume Rey Peroy <josepjaume@gmail.com>
2018-03-28 15:30:40 +0200

    Update decidim

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 
 ruby RUBY_VERSION
 
-DECIDIM_VERSION = { git: "https://github.com/decidim/decidim", branch: "0.10-stable" }
+DECIDIM_VERSION = "0.10.0"
 
 gem "decidim", DECIDIM_VERSION
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316161619) do
+ActiveRecord::Schema.define(version: 20180328132833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
