302354e17198489e1a14e3c18f900ce46dd1d837
Oriol Gual <oriol@codegram.com>
2018-12-05 10:45:30 +0100

    Enable English

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -7,7 +7,7 @@ Decidim.configure do |config|
 
   # Change these lines to set your preferred locales
   config.default_locale = :ca
-  config.available_locales = [:ca, :es]
+  config.available_locales = [:ca, :es, :en]
 
   # Geocoder configuration
   config.geocoder = {
