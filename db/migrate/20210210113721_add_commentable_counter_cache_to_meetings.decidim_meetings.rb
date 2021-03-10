0ff7ff9fc5e355f94db036b97943f12b35cd779f
Marc Riera Casals <mrc2407@gmail.com>
2021-03-10 09:58:10 +0100

    Fix migration

--- db/migrate/20210210113721_add_commentable_counter_cache_to_meetings.decidim_meetings.rb
+++ db/migrate/20210210113721_add_commentable_counter_cache_to_meetings.decidim_meetings.rb
@@ -2,6 +2,10 @@
 # This migration comes from decidim_meetings (originally 20200827153856)
 
 class AddCommentableCounterCacheToMeetings < ActiveRecord::Migration[5.2]
+  class Decidim::Meetings::Meeting < Decidim::Meetings::ApplicationRecord
+    def salt; end
+  end
+
   def change
     add_column :decidim_meetings_meetings, :comments_count, :integer, null: false, default: 0
     Decidim::Meetings::Meeting.reset_column_information
