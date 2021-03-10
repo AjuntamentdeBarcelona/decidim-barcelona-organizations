f161003f67022ced0709a1f596c642d4b807601b
Marc Riera Casals <mrc2407@gmail.com>
2021-03-10 09:12:15 +0100

    Update decidim

--- db/migrate/20210210113725_add_commentable_counter_cache_to_proposals.decidim_proposals.rb
+++ db/migrate/20210210113725_add_commentable_counter_cache_to_proposals.decidim_proposals.rb
@@ -6,7 +6,11 @@ class AddCommentableCounterCacheToProposals < ActiveRecord::Migration[5.2]
     add_column :decidim_proposals_proposals, :comments_count, :integer, null: false, default: 0, index: true
     add_column :decidim_proposals_collaborative_drafts, :comments_count, :integer, null: false, default: 0, index: true
     Decidim::Proposals::Proposal.reset_column_information
-    Decidim::Proposals::Proposal.find_each(&:update_comments_count)
+    Decidim::Proposals::Proposal.find_in_batches do |batch|
+      p "Running batch!"
+
+      batch.each(&:update_comments_count)
+    end
     Decidim::Proposals::CollaborativeDraft.reset_column_information
     Decidim::Proposals::CollaborativeDraft.find_each(&:update_comments_count)
   end
