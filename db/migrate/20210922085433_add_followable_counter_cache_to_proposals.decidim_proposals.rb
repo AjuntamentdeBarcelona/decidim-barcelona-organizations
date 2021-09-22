609ee8dd17b57a1831f26d8ea2839458b5c4cd07
Leonardo Diez <leiodd@gmail.com>
2021-09-22 13:48:22 +0200

    Don't update users and proposals follows during migrations

--- db/migrate/20210922085433_add_followable_counter_cache_to_proposals.decidim_proposals.rb
+++ db/migrate/20210922085433_add_followable_counter_cache_to_proposals.decidim_proposals.rb
@@ -5,13 +5,13 @@ class AddFollowableCounterCacheToProposals < ActiveRecord::Migration[5.2]
   def change
     add_column :decidim_proposals_proposals, :follows_count, :integer, null: false, default: 0, index: true
 
-    reversible do |dir|
-      dir.up do
-        Decidim::Proposals::Proposal.reset_column_information
-        Decidim::Proposals::Proposal.find_each do |record|
-          record.class.reset_counters(record.id, :follows)
-        end
-      end
-    end
+    # reversible do |dir|
+    #   dir.up do
+    #     Decidim::Proposals::Proposal.reset_column_information
+    #     Decidim::Proposals::Proposal.find_each do |record|
+    #       record.class.reset_counters(record.id, :follows)
+    #     end
+    #   end
+    # end
   end
 end
