7972ff69be96e9f4ee8cd7150514b00950fc50db
leio10 <leiodd@gmail.com>
2021-11-09 13:00:14 +0100

    Fix performance issue with assemblies query
    The visible_for method for Assemblies has performance issues when there are many private users for assemblies. Also, it is not needed anymore, as the HasPrivateUsers versions uses the modified private_spaces version for assemblies. Finally, it is also wrong, as it's including not published assemblies in the result, while the public_spaces doesn't do that.
    This commit redefined the method using the version from HasPrivateUsers. It can be reverted after updating to a version of Decidim that removes the visible_for method from the Assembly model.

---
+++ app/models/decidim/assembly.rb
@@ -0,0 +1,20 @@
+# Restore visible_for from the HasPrivateUsers concern
+# Remove this file after upgrading to a version of Decidim where the visible_for method of Assembly is removed
+require_dependency Decidim::Assemblies::Engine.root.join('app', 'models', 'decidim', 'assembly').to_s
+
+class Decidim::Assembly
+  def self.visible_for(user)
+    if user
+      return all if user.admin?
+
+      where(
+        id: public_spaces +
+            private_spaces
+              .joins(:participatory_space_private_users)
+              .where(decidim_participatory_space_private_users: { decidim_user_id: user.id })
+      )
+    else
+      public_spaces
+    end
+  end
+end
