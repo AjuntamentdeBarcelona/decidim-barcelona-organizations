314e5b420a803c49e9e7a04f012f9fb640d916ea
smartosr <118991050+smartosr@users.noreply.github.com>
2023-01-18 09:43:27 +0100

    Redirect a meeting component (#125)
    * meeting  component with id=2723 redirected on menu of conference
    
    * button of meeting component with id=2723in hero redirected
    
    * Add tests and lint files
    
    * Delete .rspec-failures
    
    Co-authored-by: Francisco Bolívar <francisco.bolivar@nazaries.com>

---
+++ spec/lib/overrides_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+# We make sure that the checksum of the file overriden is the same
+# as the expected. If this test fails, it means that the overriden
+# file should be updated to match any change/bug fix introduced in the core
+checksums = [
+  {
+    package: "decidim-conferences",
+    files: {
+      "/app/views/layouts/decidim/_conference_hero.html.erb" => "db2e565169a5ce7f8a3e7d03dd0ebfd9",
+      "/app/views/layouts/decidim/_conferences_nav.html.erb" => "3911afb1f1d51a933ee44e2676f6d334"
+    }
+  }
+]
+
+describe "Overriden files", type: :view do
+  checksums.each do |item|
+    # rubocop:disable Rails/DynamicFindBy
+    spec = ::Gem::Specification.find_by_name(item[:package])
+    # rubocop:enable Rails/DynamicFindBy
+    item[:files].each do |file, signature|
+      it "#{spec.gem_dir}#{file} matches checksum" do
+        expect(md5("#{spec.gem_dir}#{file}")).to eq(signature)
+      end
+    end
+  end
+
+  private
+
+  def md5(file)
+    Digest::MD5.hexdigest(File.read(file))
+  end
+end
