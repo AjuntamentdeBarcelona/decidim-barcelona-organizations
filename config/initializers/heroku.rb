d193858a06b5838860693b47cc7d91943104b73a
Alejandro Rueda <alejandro.rueda@nazaries.com>
2026-04-08 11:30:38 +0300

    Delete unused heroku initializer

--- config/initializers/heroku.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "platform-api"
-
-# rubocop:disable Style/GlobalVars
-$heroku = PlatformAPI.connect_oauth(ENV.fetch("HEROKU_OAUTH_TOKEN", nil))
-# rubocop:enable Style/GlobalVars
