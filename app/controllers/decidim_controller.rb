11f83773065781aa061e110930da56873dc8e4e4
Alejandro Rueda <alejandro.rueda@nazaries.com>
2026-06-15 18:22:44 +0300

    Remove sentry from decidim_controller.rb

--- app/controllers/decidim_controller.rb
+++ app/controllers/decidim_controller.rb
@@ -1,17 +1,5 @@
-# frozen_string_literal: true
-
 # Entry point for Decidim. It will use the `DecidimController` as
 # entry point, but you can change what controller it inherits from
 # so you can customize some methods.
 class DecidimController < ApplicationController
-  before_action :set_raven_context
-
-  private
-
-  def set_raven_context
-    return if ENV["SENTRY_ENABLED"].blank?
-
-    Raven.user_context({ id: try(:current_user).try(:id) }.merge(session))
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-  end
 end
